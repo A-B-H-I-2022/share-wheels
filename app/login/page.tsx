a6dbcd26ff29b9b502c37e9ede5094b3d1033670
Joel J George <joeljoby111@gmail.com>
2023-09-23 02:54:14 +0530

    feat: added login b2b

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,11 +3,11 @@ import { Input } from "@nextui-org/input";
 import { Button } from "@nextui-org/button";
 export default function LoginPage() {
   return (
-    <div className="bg-indigo-300">
-      <div className="h-40 flex justify-center align-middle">
-        <h1 className="text-6xl font-medium">Sign Up</h1>
+    <div className="bg-indigo-100 light">
+      <div className="flex justify-center h-40 align-middle">
+        <h1 className="text-6xl font-medium text-black">Sign Up</h1>
       </div>
-      <div className="p-10 bg-stone-900 h-full rounded-tl-5xl pt-20">
+      <div className="h-full p-10 pt-20 bg-stone-900 rounded-tl-5xl">
         <form action="" method="post" className="flex flex-col gap-3">
           <Input type="text" label="Name" />
           <Input type="email" label="Email" placeholder="Enter your email" />
