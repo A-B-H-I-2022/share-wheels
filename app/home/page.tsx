44c93d9eefa49f2796f4c9993636832e33217a3e
Joel J George <joeljoby111@gmail.com>
2023-09-24 02:44:46 +0530

    feat: added home

---
+++ app/home/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <>
+      <div className="h-screen bg-gray-300">
+        <div className="fixed bottom-0 w-full bg-indigo-400 rounded-lg h-1/2">
+          <h1>You have completed</h1>
+        </div>
+      </div>{" "}
+    </>
+  );
+}
