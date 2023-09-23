942fc2623078c60714d46a8a865678b8b342bafd
Joel J George <joeljoby111@gmail.com>
2023-09-23 21:19:36 +0530

    added images

--- app/departure-community/drive/page.tsx
+++ app/departure-community/drive/page.tsx
@@ -1,24 +1,27 @@
 "use client";
 import React from "react";
 import { Input } from "@nextui-org/input";
-import { Card, CardHeader, CardBody } from "@nextui-org/card";
+import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/card";
 import Image from "next/image";
 import { Button } from "@nextui-org/button";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 const { createClient } = require("@supabase/supabase-js");
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+} from "@nextui-org/modal";
 interface Office {
   name: string;
   image: string;
   address: string;
   description: string;
 }
-interface Home {
-  name: string;
-  image: string;
-  address: string;
-  description: string;
-}
+
 interface OfficePlace {
   id: number;
   created_at: Date | null;
@@ -26,98 +29,204 @@ interface OfficePlace {
   image: string | null;
   address: string | null;
 }
+interface Home {
+  id: number;
+  created_at: Date;
+  name: string | null;
+  address: string | null;
+  description: string | null;
+  image: string | null;
+}
+interface Driveorride {
+  image: string;
+  title: string;
+  route: string;
+}
 export default function DC() {
   const [Community, setCommunity] = React.useState("InfoPark");
-  const [Home, setHome] = React.useState("Skyline");
+  const [home, setHome] = React.useState("Skyline");
   const supabase = createClient(supabaseUrl, supabaseKey);
   const [tryoffice, setTryoffice] = React.useState<OfficePlace[]>([]);
-  const people: Office[] = [
-    {
-      name: "InfoPark",
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/1/1b/Infopark%2C_Kakkanad%2C_Kochi%2C_Kerala%2C_India.jpg",
-      address: "Kochi, Kerala, India",
-      description: "Koc",
-    },
-    {
-      name: "Technopark",
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Technopark_Trivandrum_Ariel_View_June_2014.jpg/435px-Technopark_Trivandrum_Ariel_View_June_2014.jpg",
-      address: "Trivandrum, Kerala, India",
-      description: "Koc",
-    },
+  const [check, setCheck] = React.useState(true);
+  const [tryhome, setTryhome] = React.useState<Home[]>([]);
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const List = [
+    { image: "/images/rider .png", title: "Ride", route: "/ride" },
+    { image: "/images/steering.png", title: "Drive", route: "/drive" },
   ];
-  const home: Home[] = [
-    {
-      name: "Skyline",
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/5/5f/Cityscape_view_from_Kakkanad.jpg",
-      address: "Kakkanad, Kerala, India",
-      description: "",
-    },
-  ];
-  React.useEffect(() => {
-    async function fetchB2CData() {
-      let { data, error } = await supabase.from("b2c").select("*");
-      if (error) {
-        console.log("error", error);
-      } else {
-        console.log("b2c", data);
-      }
-    }
-    async function fetchPlaceData() {
-      const { data, error } = await supabase.from("officeplace").select("*");
-      if (error) {
-        console.log("error", error);
-      } else {
-        console.log("place", data);
-      }
-      setTryoffice(data);
-    }
+  // React.useEffect(() => {
+  //   // async function fetchB2CData() {
+  //   //   let { data, error } = await supabase.from("b2c").select("*");
+  //   //   if (error) {
+  //   //     console.log("error", error);
+  //   //   } else {
+  //   //     console.log("b2c", data);
+  //   //   }
+  //   // }
+  //   async function fetchPlaceData() {
+  //     const { data, error } = await supabase.from("officeplace").select("*");
+  //     if (error) {
+  //       console.log("error", error);
+  //     } else {
+  //       console.log("place", data);
+  //     }
+  //     setTryoffice(data);
+  //   }
 
-    fetchB2CData();
-    fetchPlaceData();
-  }, []);
+  //   // fetchB2CData();
+  // }, []);
+
+  React.useEffect(() => {
+    async function fetchHomeData(home: string) {
+      let { data, error } = await supabase
+        .from("Home")
+        .select("*")
+        .ilike("name", `%${home}%`);
+      if (error) {
+        console.error(error);
+      } else {
+        setTryhome(data);
+        console.log(data);
+      }
+    }
+    fetchHomeData(home);
+  }, [home]);
+  React.useEffect(() => {
+    async function fetchPlaceData(community: string) {
+      let { data, error } = await supabase
+        .from("officeplace")
+        .select("*")
+        .ilike("name", `%${community}%`);
+      if (error) {
+        console.error(error);
+      } else {
+        setTryoffice(data);
+      }
+    }
+    fetchPlaceData(Community);
+  }, [Community]);
+  function handleCommunityChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setCommunity(event.target.value);
+  }
+  function handleHomeChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setHome(event.target.value);
+  }
 
   return (
     <>
       <div className="flex flex-col gap-4 p-4 pt-10 bg-indigo-50">
-        <form action="" className="flex flex-row">
-          <Input
-            type="text"
-            label="Accomodation Community"
-            placeholder="Enter the name of your apartment complex"
-          />
-          <Button color="success" className="h-full">
-            Submit
-          </Button>
-        </form>
+        <Input
+          type="text"
+          label="Accomodation Community"
+          placeholder="Enter the name of your apartment complex"
+          value={Community}
+          onChange={handleCommunityChange}
+          onClick={() => {
+            setCheck(true);
+          }}
+        />
         <Input
           type="text"
           label="Office Community"
           placeholder="Enter the name of your Office commute"
+          value={home}
+          onChange={handleHomeChange}
         />
-        {tryoffice
-          .filter((person) => person.name === Community)
-          .map((person, index) => (
-            <Card className="py-4" key={index}>
-              <CardHeader className="flex-col items-start px-4 pt-2 pb-0">
-                <h4 className="font-bold text-large">{person.name}</h4>
-                <p className="font-bold uppercase text-tiny">
-                  {person.address}
-                </p>
-              </CardHeader>
-              <CardBody className="py-2 overflow-visible">
-                <Image
-                  alt="Card background"
-                  className="object-cover rounded-xl"
-                  src={person.image ?? ""}
-                  width={500}
-                  height={500}
-                />
-              </CardBody>
-            </Card>
+        {check &&
+          tryoffice.map((person, index) => (
+            <div
+              className=""
+              key={index}
+              onClick={() => {
+                setCommunity(person.name ?? "");
+                setCheck(false);
+              }}
+            >
+              <Card className="py-4 cursor-pointer">
+                <CardHeader className="flex-col items-start px-4 pt-2 pb-0">
+                  <h4 className="font-bold text-large">{person.name}</h4>
+                  <p className="font-bold uppercase text-tiny">
+                    {person.address}
+                  </p>
+                </CardHeader>
+                <CardBody className="py-2 overflow-visible">
+                  <Image
+                    alt="Card background"
+                    className="object-cover rounded-xl"
+                    src={person.image ?? ""}
+                    width={500}
+                    height={500}
+                  />
+                </CardBody>
+              </Card>
+            </div>
           ))}
+        {!check &&
+          tryhome.map((homie, index) => (
+            <div
+              className=""
+              key={index}
+              onClick={() => {
+                setHome(homie.name ?? "");
+              }}
+            >
+              <Card className="py-4 cursor-pointer">
+                <CardHeader className="flex-col items-start px-4 pt-2 pb-0">
+                  <h4 className="font-bold text-large">{homie.name}</h4>
+                  <p className="font-bold uppercase text-tiny">
+                    {homie.address}
+                  </p>
+                </CardHeader>
+                <CardBody className="py-2 overflow-visible">
+                  <Image
+                    alt="Card background"
+                    className="object-cover rounded-xl"
+                    src={homie.image ?? ""}
+                    width={500}
+                    height={500}
+                  />
+                </CardBody>
+              </Card>
+            </div>
+          ))}
+        <Button onPress={onOpen}>Submit</Button>
+        <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+          <ModalContent>
+            {(onClose) => (
+              <>
+                <ModalHeader className="flex flex-col gap-1">
+                  Choose ride or drive
+                </ModalHeader>
+                <ModalBody>
+                  <div className="flex gap-2">
+                    {List.map((item, index) => (
+                      <Card
+                        shadow="sm"
+                        key={index}
+                        className="light"
+                        isPressable
+                        onPress={() => console.log("item pressed")}
+                      >
+                        <CardBody className="p-0 overflow-visible">
+                          <Image
+                            width={100}
+                            height={100}
+                            alt={item.title}
+                            className="object-center "
+                            src={item.image}
+                          />
+                        </CardBody>
+                        <CardFooter className="justify-between text-small">
+                          <b>{item.title}</b>
+                        </CardFooter>
+                      </Card>
+                    ))}
+                  </div>
+                </ModalBody>
+              </>
+            )}
+          </ModalContent>
+        </Modal>
       </div>
     </>
   );
